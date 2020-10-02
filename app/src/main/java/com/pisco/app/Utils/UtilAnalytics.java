e6e845565122cfebba476652136702c23199c2e9
hugo <hugo.fisi@gmail.com>
2020-10-01 21:38:52 -0500

    tag manager

--- app/src/main/java/com/pisco/app/Utils/UtilAnalytics.java
+++ app/src/main/java/com/pisco/app/Utils/UtilAnalytics.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 
 import com.pisco.app.PiscoApplication;
 
+import timber.log.Timber;
+
 public class UtilAnalytics {
 
     public static void sendEvent(PiscoApplication application, String event, String hitType, String eventCategory, String eventAction, String eventLabel) {
-        Bundle bundle = new Bundle();
+        Timber.i("analytics disable");
+        /*Bundle bundle = new Bundle();
         bundle.putString("hitType", hitType);
         bundle.putString("eventCategory", eventCategory);
         bundle.putString("eventAction", eventAction);
         bundle.putString("eventLabel", eventLabel);
-        application.firebaseAnalytics.logEvent(event, bundle);
+        application.firebaseAnalytics.logEvent(event, bundle);*/
     }
 }
